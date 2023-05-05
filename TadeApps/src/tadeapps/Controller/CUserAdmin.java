9ab87f8cf4f74b339c066e7dc3f94e63ce6c7989
Ranaufal <115529557+Ranaufal@users.noreply.github.com>
2023-05-05 14:12:47 +0700

    Add files via upload

---
+++ TadeApps/src/tadeapps/Controller/CUserAdmin.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tadeapps.Controller;
+
+import tadeapps.View.VUserAdmin;
+//import View.ViewUpdateAdmin;
+import tadeapps.Model.MAdmin;
+import tadeapps.Dao.DUserAdmin;
+import Koneksi.Koneksi;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author MSI-PC
+ */
+public class CUserAdmin {
+    VUserAdmin view;
+//    ViewUpdateAdmin viewUpdate;
+    MAdmin model;
+    DUserAdmin dao;
+    Koneksi k;
+    private int n = 1;
+    
+    public CUserAdmin(VUserAdmin view) {
+        this.view = view;
+    }
+
+    
+//    public void update() throws ClassNotFoundException {
+//        model = new MAdmin(viewUpdate.getTxtUsername().getText(),
+//                viewUpdate.getTxtPassword().getText(),
+//                viewUpdate.getTxtNamaPegawai().getText(),
+//                viewUpdate.getTxtNoHp().getText(),
+//                viewUpdate.getJk(),
+//                viewUpdate.getTxtAlamat().getText());
+//        dao = new DUserAdmin();
+//        k = new Koneksi();
+//        try {
+//            dao.queryUpdate(k.getKoneksi(), model);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(CUserAdmin.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//    
+    public void hapus() throws ClassNotFoundException {
+        DefaultTableModel tabel = (DefaultTableModel)view.getjTableAdmin().getModel();
+        model = new MAdmin();
+        int index = view.getjTableAdmin().getSelectedRow();
+        model.setUsername(tabel.getValueAt(index, 1).toString());
+        dao = new DUserAdmin();
+        k = new Koneksi();
+        try {
+            int id = dao.queryId(k.getKoneksi(), model.getUsername());
+            dao.queryDelete(k.getKoneksi(), id);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+//    
+    public void viewTabel() throws ClassNotFoundException {
+        DefaultTableModel tabel = (DefaultTableModel)view.getjTableAdmin().getModel();
+        tabel.setRowCount(0);
+        dao = new DUserAdmin();
+        k = new Koneksi();
+        List<MAdmin> list;
+        try {
+            list = dao.queryAllData(k.getKoneksi());
+            for (MAdmin model : list) {
+                Object data[] = {
+                    n++,
+                    model.getUsername(),
+                    model.getPassword(),
+                    model.getNama_pegawai(),
+                    model.getNo_hp(),
+                    model.getJk(),
+                    model.getAlamat(),
+                    model.getLevel_akses()
+                };
+                tabel.addRow(data);
+            }
+            n = 1;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+//    
+//    public void setUpdate() throws ClassNotFoundException {
+//        DefaultTableModel tabel = (DefaultTableModel)view.getTabelAdmin().getModel();
+//        model = new ModelAdmin();
+//        dao = new DAOAdmin();
+//        k = new Koneksi();
+//        int index = view.getTabelAdmin().getSelectedRow();
+//        model.setUsername(tabel.getValueAt(index, 1).toString());
+//        try {
+//            dao.queryId(k.getKoneksi(), model.getUsername());
+//            model = dao.querySingleData(k.getKoneksi(), model.getId_admin());
+//            viewUpdate.getTxtUsername().setText(model.getUsername());
+//            viewUpdate.getTxtPassword().setText(model.getPassword());
+//            viewUpdate.getTxtNamaPegawai().setText(model.getNama_pegawai());
+//            viewUpdate.getTxtNoHp().setText(model.getNo_hp());
+////            viewUpdate.getJk().setText(model.getUsername());
+//            viewUpdate.getTxtUsername().setText(model.getUsername());
+//        } catch (SQLException ex) {
+//            Logger.getLogger(CUserAdmin.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+            
+}
---
+++ TadeApps/src/tadeapps/Dao/DUserAdmin.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tadeapps.Dao;
+
+import tadeapps.Model.MAdmin;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author MSI-PC
+ */
+public class DUserAdmin {
+    public List<MAdmin> queryAllData(Connection con) throws SQLException {
+        String sql = "SELECT * FROM admin";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        List<MAdmin> list = new ArrayList<>();
+        while (rs.next()) {
+            MAdmin model = new MAdmin(rs.getInt(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6),
+                    rs.getString(7),
+                    rs.getString(8));
+            list.add(model);
+        }
+        return list;
+    }
+    
+    public void queryUpdate(Connection con, MAdmin model) throws SQLException {
+        String sql = "UPDATE admin SET username=?, password=?, nama_pegawai=?, no_hp=?, jk=?, alamat=?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, model.getUsername());
+        ps.setString(2, model.getPassword());
+        ps.setString(3, model.getNama_pegawai());
+        ps.setString(4, model.getNo_hp());
+        ps.setString(5, model.getJk());
+        ps.setString(6, model.getAlamat());
+        ps.executeUpdate();
+    }
+    
+    public void queryDelete(Connection con, int id) throws SQLException {
+        String sql = "DELETE FROM admin WHERE id_admin=?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ps.executeUpdate();
+    }
+    
+    public int queryId(Connection con, String username) throws SQLException {
+        MAdmin model = new MAdmin();
+        String sql = "SELECT * FROM admin WHERE username=?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, username);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {            
+            model.setId_admin(rs.getInt("id_admin"));
+        }
+        return model.getId_admin();
+    }
+    
+    public MAdmin querySingleData(Connection con, int id) throws SQLException {
+        String sql = "SELECT * FROM admin WHERE id_admin=?";
+        MAdmin model = null;
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            model = new MAdmin(rs.getString("username"),
+                    rs.getString("password"),
+                    rs.getString("nama_pegawai"),
+                    rs.getString("no_hp"),
+                    rs.getString("jk"),
+                    rs.getString("alamat"));
+        }
+        return model;
+    }
+}
